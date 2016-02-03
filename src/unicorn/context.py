af1b7ec16ef835b984ed332848ea4996e5500ad0
Christian Lölkes <loelkes@Christians-MBP.dyn.club.entropia.de>
2016-02-04 00:36:44 +0100

    Adapted GCODE output for Marlin Firmware

--- src/unicorn/context.py
+++ src/unicorn/context.py
@@ -17,70 +17,73 @@ class GCodeContext:
       self.num_pages = num_pages
       self.continuous = continuous
       self.file = file
-      
+
       self.drawing = False
       self.last = None
 
       self.preamble = [
-        "(Scribbled version of %s @ %.2f)" % (self.file, self.xy_feedrate),
+        "                       ; (Scribbled version of %s @ %.2f)" % (self.file, self.xy_feedrate),
         "( %s )" % " ".join(sys.argv),
-        "G21 (metric ftw)",
-        "G90 (absolute mode)",
-        "G92 X%.2f Y%.2f Z%.2f (you are here)" % (self.x_home, self.y_home, self.z_height),
+        "G21                    ; (metric ftw)",
+        "G90                    ; Absolute mode",
+        "G92 X%.2f Y%.2f Z%.2f  ; You are here" % (self.x_home, self.y_home, self.z_height),
         ""
       ]
 
       self.postscript = [
         "",
-				"(end of print job)",
-				"M300 S%0.2F (pen up)" % self.pen_up_angle,
-				"G4 P%d (wait %dms)" % (self.stop_delay, self.stop_delay),
-				"M300 S255 (turn off servo)",
+				"                           ; (end of print job)",
+                "M400                       ; Wait until all actions are finished",
+				"M300 S%0.2F                ; Pen up" % self.pen_up_angle,
+				"G4 P%d                     ; Wait %dms" % (self.stop_delay, self.stop_delay),
+                # Note: do not turn the srvo off because the pen falls on the object
+			    #"M300 S255                  ; (turn off servo)",
 				"G1 X0 Y0 F%0.2F" % self.xy_feedrate,
-				"G1 Z%0.2F F%0.2F (go up to finished level)" % (self.finished_height, self.z_feedrate),
-				"G1 X%0.2F Y%0.2F F%0.2F (go home)" % (self.x_home, self.y_home, self.xy_feedrate),
-				"M18 (drives off)",
+				# "G1 Z%0.2F F%0.2F           ; Go up to finished level" % (self.finished_height, self.z_feedrate),
+				"G1 X%0.2F Y%0.2F F%0.2F    ; Go home" % (self.x_home, self.y_home, self.xy_feedrate),
+				"M18                        ; Drives off",
       ]
 
       self.registration = [
-        "M300 S%d (pen down)" % (self.pen_down_angle),
-        "G4 P%d (wait %dms)" % (self.start_delay, self.start_delay),
-        "M300 S%d (pen up)" % (self.pen_up_angle),
-        "G4 P%d (wait %dms)" % (self.stop_delay, self.stop_delay),
-        "M18 (disengage drives)",
-        "M01 (Was registration test successful?)",
-        "M17 (engage drives if YES, and continue)",
+        "M300 S%d       ; Pen down" % (self.pen_down_angle),
+        "G4 P%d         ; Wait %dms" % (self.start_delay, self.start_delay),
+        "M300 S%d       ; Pen up" % (self.pen_up_angle),
+        "G4 P%d         ; Wait %dms" % (self.stop_delay, self.stop_delay),
+        "M18            ; Disengage drives",
+        "M01            ; (Was registration test successful?)",
+        "M17            ; (engage drives if YES, and continue)",
         ""
       ]
 
       self.sheet_header = [
-        "(start of sheet header)",
-        "G92 X%.2f Y%.2f Z%.2f (you are here)" % (self.x_home, self.y_home, self.z_height),
+        "                       ; (start of sheet header)",
+        "G92 X%.2f Y%.2f Z%.2f  ; You are here" % (self.x_home, self.y_home, self.z_height),
       ]
       if self.register_pen == 'true':
         self.sheet_header.extend(self.registration)
-      self.sheet_header.append("(end of sheet header)")
+      self.sheet_header.append("; End of sheet header")
 
       self.sheet_footer = [
-        "(Start of sheet footer.)",
-        "M300 S%d (pen up)" % (self.pen_up_angle),
-        "G4 P%d (wait %dms)" % (self.stop_delay, self.stop_delay),
-        "G91 (relative mode)",
+        "; (Start of sheet footer.)",
+        "M400               ; Wait until all action finished",
+        "M300 S%d           ; Pen up" % (self.pen_up_angle),
+        "G4 P%d             ; Wait %dms" % (self.stop_delay, self.stop_delay),
+        "G91                ; Relative mode",
         "G0 Z15 F%0.2f" % (self.z_feedrate),
-        "G90 (absolute mode)",
+        "G90                ; Absolute mode",
         "G0 X%0.2f Y%0.2f F%0.2f" % (self.x_home, self.y_home, self.xy_feedrate),
-        "M01 (Have you retrieved the print?)",
-        "(machine halts until 'okay')",
-        "G4 P%d (wait %dms)" % (self.start_delay, self.start_delay),
-        "G91 (relative mode)",
-        "G0 Z-15 F%0.2f (return to start position of current sheet)" % (self.z_feedrate),
-        "G0 Z-0.01 F%0.2f (move down one sheet)" % (self.z_feedrate),
-        "G90 (absolute mode)",
-        "M18 (disengage drives)",
-        "(End of sheet footer)",
+        "M01                ; (Have you retrieved the print?)",
+        "                   ; (machine halts until 'okay')",
+        "G4 P%d             ; Wait %dms" % (self.start_delay, self.start_delay),
+        "G91                ; Relative mode",
+        # "G0 Z-15 F%0.2f     ; Return to start position of current sheet" % (self.z_feedrate),
+        # "G0 Z-0.01 F%0.2f   ; Move down one sheet" % (self.z_feedrate),
+        "G90                ; Absolute mode",
+        "M18                ; Disengage drives",
+        "; (End of sheet footer)",
       ]
 
-      self.loop_forever = [ "M30 (Plot again?)" ]
+      self.loop_forever = [ "M30 ; (Plot again?)" ]
 
       self.codes = []
 
@@ -111,13 +114,15 @@ class GCodeContext:
             print line
 
     def start(self):
-      self.codes.append("M300 S%0.2F (pen down)" % self.pen_down_angle)
-      self.codes.append("G4 P%d (wait %dms)" % (self.start_delay, self.start_delay))
+      self.codes.append("M400 ; Finish all moves")
+      self.codes.append("M300 S%0.2F ; pen down" % self.pen_down_angle)
+      self.codes.append("G4 P%d ; Wait %dms" % (self.start_delay, self.start_delay))
       self.drawing = True
 
     def stop(self):
-      self.codes.append("M300 S%0.2F (pen up)" % self.pen_up_angle)
-      self.codes.append("G4 P%d (wait %dms)" % (self.stop_delay, self.stop_delay))
+      self.codes.append("M400 ; Finish all moves")
+      self.codes.append("M300 S%0.2F ; pen up" % self.pen_up_angle)
+      self.codes.append("G4 P%d ; wait %dms" % (self.stop_delay, self.stop_delay))
       self.drawing = False
 
     def go_to_point(self, x, y, stop=False):
@@ -126,13 +131,14 @@ class GCodeContext:
       if stop:
         return
       else:
-        if self.drawing: 
-            self.codes.append("M300 S%0.2F (pen up)" % self.pen_up_angle) 
-            self.codes.append("G4 P%d (wait %dms)" % (self.stop_delay, self.stop_delay))
+        if self.drawing:
+            self.codes.append("M400 ; Finish all moves")
+            self.codes.append("M300 S%0.2F ; Pen up" % self.pen_up_angle)
+            self.codes.append("G4 P%d ; Wait %dms" % (self.stop_delay, self.stop_delay))
             self.drawing = False
         self.codes.append("G1 X%.2f Y%.2f F%.2f" % (x,y, self.xy_feedrate))
       self.last = (x,y)
-	
+
     def draw_to_point(self, x, y, stop=False):
       if self.last == (x,y):
           return
@@ -140,8 +146,9 @@ class GCodeContext:
         return
       else:
         if self.drawing == False:
-            self.codes.append("M300 S%0.2F (pen down)" % self.pen_up_angle)
-            self.codes.append("G4 P%d (wait %dms)" % (self.start_delay, self.start_delay))
+            self.codes.append("M400 ; Finish all moves")
+            self.codes.append("M300 S%0.2F Pen down" % self.pen_up_angle)
+            self.codes.append("G4 P%d ; Wait %dms" % (self.start_delay, self.start_delay))
             self.drawing = True
         self.codes.append("G1 X%0.2f Y%0.2f F%0.2f" % (x,y, self.xy_feedrate))
       self.last = (x,y)
--- src/unicorn/svg_parser.py
+++ src/unicorn/svg_parser.py
@@ -162,7 +162,7 @@ class SvgEllipse(SvgPath):
     newpath = self.new_path_from_node(node)
     newpath.set('d',d)
     return newpath
-  
+
 class SvgCircle(SvgEllipse):
   def load(self, node,mat):
     rx = float(node.get('r','0'))
@@ -227,12 +227,12 @@ class SvgParser:
 
   def parse(self):
     # 0.28222 scale determined by comparing pixels-per-mm in a default Inkscape file.
-    self.svgWidth = self.getLength('width', 354) * 0.28222
-    self.svgHeight = self.getLength('height', 354) * 0.28222
-    self.recursivelyTraverseSvg(self.svg, [[0.28222, 0.0, -(self.svgWidth/2.0)], [0.0, -0.28222, (self.svgHeight/2.0)]])
+    self.svgWidth = self.getLength('width', 3200) * 1
+    self.svgHeight = self.getLength('height', 800) * 1
+    self.recursivelyTraverseSvg(self.svg, [[1, 0.0, -(self.svgWidth/2.0)], [0.0, -1, (self.svgHeight/2.0)]])
 
   # TODO: center this thing
-  def recursivelyTraverseSvg(self, nodeList, 
+  def recursivelyTraverseSvg(self, nodeList,
                              matCurrent = [[1.0, 0.0, 0.0], [0.0, -1.0, 0.0]],
                              parent_visibility = 'visible'):
     """
